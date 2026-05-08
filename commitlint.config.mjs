4fc25fcb9fb7895a11d4d8b6c5aee9dc766ec6c0
Van Nguyen <nguyenthanhvan1902@gmail.com>
2026-05-08 22:14:48 +0700

    chore: increase commitlint header max length to 100

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,7 +1,7 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'header-max-length': [2, 'always', 72],
+    'header-max-length': [2, 'always', 100],
     'type-enum': [
       2,
       'always',
